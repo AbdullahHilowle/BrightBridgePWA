27e02a900bbe8e4452317710574e1be1b57de3ae
Atlus501 <helloworld4846@gmail.com>
2026-04-13 22:02:42 -0700

    naming changes

--- public/js/db.js
+++ public/js/db.js
@@ -6,29 +6,14 @@ const SUPABASE_ANON_KEY = "sb_publishable_TqBEuyZvC51pHJDVXAGk3Q_FigwHhN3"
 //class for managing the database
 const db = {
 
+    //date attribute for future operations
+    today : new Date(),
+
     // Create a single supabase client for interacting with your database
     supabase : createClient(
         SUPABASE_URL,
         SUPABASE_ANON_KEY
     ),
-
-    //this function inserts a user into the database
-    //updated database. it should be emails just to make everything easier
-    insertUser : async function(email) {
-        const { data, error } = await this.supabase
-        .from("users")
-        .insert({ name: email })
-        .select()
-
-        if (error) {
-            console.error("Error inserting data:", error.message)
-            return error
-        }
-
-        // Use console.log for JS
-        console.log("Success:", data)
-        return data
-    },
 
     //this function gets the user information based on their email
     getUserId: async function(email){
