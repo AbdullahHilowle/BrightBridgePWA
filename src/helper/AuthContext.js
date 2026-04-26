aff905e20c9190a43678547fca87688b9f074e0c
Atlus501 <helloworld4846@gmail.com>
2026-04-25 20:48:46 -0700

    add

--- src/helper/AuthContext.js
+++ src/helper/AuthContext.js
@@ -24,8 +24,8 @@ export const AuthProvider = ({ children }) => {
         // 3. LISTENERS: These update the state automatically
         window.netlifyIdentity.on('login', (loggedUser) => {
             setUser(loggedUser); // This triggers UI updates everywhere!
-            window.netlifyIdentity.close();
             localStorage.setItem('brightbridge.user', JSON.stringify(loggedUser));
+            window.netlifyIdentity.close();
         });
 
         window.netlifyIdentity.on('logout', () => {
